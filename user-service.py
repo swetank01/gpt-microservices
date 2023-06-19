725774d16c03c049cd5fe6dab44a9d5f4160c8ff
swetank01 <iamswetanksoni@gmail.com>
2023-06-19 14:06:33 +0530

    add user service with db

--- user-service.py
+++ user-service.py
@@ -1,34 +1,68 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+engine = create_engine('sqlite:///user.db', echo=True)  # SQLite database file
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True)
+    email = Column(String(100), unique=True)
+    password = Column(String(100))
+
+Base.metadata.create_all(bind=engine)  # Create tables in the database
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    session = Session()
+    users = session.query(User).all()
+
+    user_list = []
+    for user in users:
+        user_dict = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email
+        }
+        user_list.append(user_dict)
+
+    return jsonify(user_list)
 
 @app.route('/register', methods=['POST'])
 def register_user():
-    # Code for user registration
     # Retrieve user details from request data
     username = request.json['username']
     email = request.json['email']
     password = request.json['password']
 
-    # Save user to database or perform other necessary operations
-    # ...
+    session = Session()
+    user = User(username=username, email=email, password=password)
+    session.add(user)
+    session.commit()
 
     return 'User registered successfully'
 
 @app.route('/login', methods=['POST'])
 def login_user():
-    # Code for user authentication
     # Retrieve user credentials from request data
     username = request.json['username']
     password = request.json['password']
 
-    # Perform authentication logic
-    if username == 'john_doe' and password == '123456':
-        authentication_successful = True
-    else:
-        authentication_successful = False
+    session = Session()
+    user = session.query(User).filter_by(username=username, password=password).first()
 
-    if authentication_successful:
+    if user:
         return 'Login successful'
     else:
         return 'Login failed'
